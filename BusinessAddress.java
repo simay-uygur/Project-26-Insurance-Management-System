49d7843013ff338aba80a8296d4890e397c6d466
simay-uygur <simayuygur09@gmail.com>
2024-08-06 13:12:16 +0300

    address classes are done

--- BusinessAddress.java
+++ BusinessAddress.java
@@ -2,11 +2,64 @@
  * BusinessAddress
  */
 public class BusinessAddress implements Address{
+    private String companyName;
+    private String attentionLine;
+    private String streetAddress;
+    private String suitNumber;
+    private String city;
+    private String state_province_region;
+    private int postalCode;
+    private String country;
 
     @Override
     public String getAddressName() {
-        return "Business Address" + this. ; //implement later
+        return "Business Address" + companyName + ", " + attentionLine + ", "
+        + streetAddress + ", " + suitNumber + ", " + city + ", "
+        + state_province_region + ", " + postalCode + ", " + country + "." ;
         
+    }
+    public BusinessAddress(String companyName, String attentionLine, String streetAddress, String suitNumber,
+            String city, String state_province_region, int postalCode, String country) {
+        this.companyName = companyName;
+        this.attentionLine = attentionLine;
+        this.streetAddress = streetAddress;
+        this.suitNumber = suitNumber;
+        this.city = city;
+        this.state_province_region = state_province_region;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getAttentionLine() {
+        return attentionLine;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getSuitNumber() {
+        return suitNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState_province_region() {
+        return state_province_region;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCountry() {
+        return country;
     }
 
     
--- HomeAddress.java
+++ HomeAddress.java
@@ -1,9 +1,63 @@
 public class HomeAddress implements Address{
 
+    private String ownerName;
+    private String streetAddress;
+    private String apt_unitNumber;
+    private String city;
+    private String state_province_region;
+    private int postalCode;
+    private String country;
+
     @Override
     public String getAddressName() {
-       
-        throw new UnsupportedOperationException("Unimplemented method 'getAddressName'");
+       return ownerName + ", " +
+                streetAddress + ", " +
+                apt_unitNumber + ", " + 
+                city + ", " +
+                state_province_region + ", " +
+                postalCode + ", " +
+                country + ".";
     }
+
+    public HomeAddress(String ownerName, String streetAddress, String apt_unitNumber, String city,
+            String state_province_region, int postalCode, String country) {
+        this.ownerName = ownerName;
+        this.streetAddress = streetAddress;
+        this.apt_unitNumber = apt_unitNumber;
+        this.city = city;
+        this.state_province_region = state_province_region;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getApt_unitNumber() {
+        return apt_unitNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState_province_region() {
+        return state_province_region;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    
     
 }
